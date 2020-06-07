760cf5a0d779b04efbcde6f301bb0cc4c55bef92
JaneGirshin <Jane@girsh.in>
2020-06-07 16:04:27 +0300

    Creating game

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import './App.css';
+import Hangman from './components/Hangman'
 
 function App() {
   return (
     <div className="App">
-      <h1>Hello world</h1>
+      <Hangman />
     </div>
   );
 }
